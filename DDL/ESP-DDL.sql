523b0a41e14d9f980c9eb97fd1bedeef99ed589c
Dan Gilleland <dgilleland@users.noreply.github.com>
2018-10-01 10:45:23 -0600

    Create tables with PK and FK

---
+++ DDL/ESP-DDL.sql
@@ -0,0 +1,78 @@
+/* **********************************************
+ * Simple Table Creation - Columns and Primary Keys
+ *
+ * Emergency Service & Product
+ * Specification Document 1
+ * Version 1.0.0
+ *
+ * Author: Dan Gilleland
+ ********************************************** */
+
+-- Select the CREATE DATABASE stement below to create the demo database.
+-- CREATE DATABASE [ESP-A01]
+
+USE [ESP-A01] -- this is a statement that tells us to switch to a particular database
+-- Notice in the database name above, it is "wrapped" in square brackets because 
+-- the name had a hypen in it. 
+-- For all our other objects (tables, columns, etc), we won't use hypens or spaces, so
+-- the use of square brackets are optional.
+GO  -- this statement helps to "separate" various DDL statements in our script
+    -- so that they are executed as "blocks" of code.
+
+-- To create a database table, we use the CREATE TABLE statement.
+-- Note that the order in which we create/drop tables is important
+-- because of how the tables are related via Foreign Keys.
+
+/* DROP TABLE statements (to "clean up" the database for re-creation)  */
+/*   You should drop tables in the REVERSE order in which you created them */
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Orders')
+    DROP TABLE Orders
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+    DROP TABLE Customers
+
+
+-- To create a database table, we use the CREATE TABLE statement.
+-- Note that the order in which we create/drop tables is important
+-- because of how the tables are related via Foreign Keys.
+CREATE TABLE Customers
+(
+    -- The body of a CREATE TABLE will identify a comma-separated list of
+    -- Column Declarations and Table Constraints.
+    CustomerNumber  int
+        -- The following is a PRIMARY KEY constraint that has a specific name
+        -- Primary Key constraints ensure a row of data being added to the table
+        -- will have to have a unique value for the Primary Key column(s)
+        CONSTRAINT PK_Customers_CustomerNumber
+            PRIMARY KEY
+        -- IDENTITY means the database will generate a unique whole-number
+        -- value for this column
+        IDENTITY(100, 1) -- The first number is the "seed",
+                         -- and the last number is the "increment"
+                                        NOT NULL, -- NOT NULL means the data is required
+    FirstName       varchar(50)         NOT NULL,
+    LastName        varchar(60)         NOT NULL,
+    [Address]       varchar(40)         NOT NULL,
+    City            varchar(35)         NOT NULL,
+    Province        char(2)             NOT NULL,
+    PostalCode      char(6)             NOT NULL,
+    PhoneNumber     char(13)                NULL  -- NULL means the data is optional
+)
+
+CREATE TABLE Orders
+(
+    OrderNumber     int
+        CONSTRAINT PK_Orders_OrderNumber
+            PRIMARY KEY
+        IDENTITY(200, 1)                NOT NULL,
+    CustomerNumber  int
+        -- Foreign Key constraints ensure that when a row of data is being
+        -- inserted or updated, there is a row in the referenced table
+        -- that has the same value as its Primary Key
+        CONSTRAINT FK_Orders_CustomerNumber_Customers_CustomerNumber
+            FOREIGN KEY REFERENCES
+            Customers(CustomerNumber)   NOT NULL,
+    [Date]          datetime            NOT NULL,
+    Subtotal        money               NOT NULL,
+    GST             money               NOT NULL,
+    Total           money               NOT NULL
+)
