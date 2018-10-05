be0e37f97c47bdcefc821cc79e6da41d3963e76b
Dan Gilleland <dgilleland@users.noreply.github.com>
2018-10-05 09:13:27 -0600

    Additional alter + indexes

--- DDL/ESP-DDL.sql
+++ DDL/ESP-DDL.sql
@@ -215,9 +215,142 @@ ALTER TABLE Customers
     ALTER COLUMN PostalCode char(6) NULL
 GO
 
-
-
 -- B) Add a check constraint on the First and Last name to require at least two letters.
 --    % is a wildcard for zero or more characters (letter, digit, or other character)
 --    _ is a wildcard for a single character (letter, digit, or other character)
 --    [] are used to represent a range or set of characters that are allowed
+IF OBJECT_ID('CK_Customers_FirstName', 'C') IS NOT NULL -- 'C' specifies that I'm looking for a constraint
+    ALTER TABLE Customers DROP CONSTRAINT CK_Customers_FirstName
+
+ALTER TABLE Customers
+    ADD CONSTRAINT CK_Customers_FirstName
+        CHECK (FirstName LIKE '[A-Z][A-Z]%')
+
+IF OBJECT_ID('CK_Customers_LastName', 'C') IS NOT NULL
+    ALTER TABLE Customers DROP CONSTRAINT CK_Customers_LastName
+
+ALTER TABLE Customers
+    ADD CONSTRAINT CK_Customers_LastName
+        CHECK (LastName LIKE '[A-Z][A-Z]%')
+
+-- Once the ALTER TABLE changes are made for A) and B),
+-- we can insert Customer information allowing certain columns to be NULL.
+INSERT INTO Customers(FirstName, LastName)
+    VALUES ('Fred', 'Flintstone')
+INSERT INTO Customers(FirstName, LastName)
+    VALUES ('Barney', 'Rubble')
+INSERT INTO Customers(FirstName, LastName, PhoneNumber)
+    VALUES ('Wilma', 'Slaghoople', '(403)555-1212')
+INSERT INTO Customers(FirstName, LastName, [Address], City)
+    VALUES ('Betty', 'Mcbricker', '103 Granite Road', 'Bedrock')
+
+-- Select the customer information
+SELECT  CustomerNumber, FirstName, LastName,
+        [Address] + ' ' + City + ', ' + Province AS 'Customer Address',
+        PhoneNumber
+FROM    Customers
+GO
+
+/*
+    You can check that the constraints work on the first/last name
+    by highlighting and running these scripts. They should fail.
+
+INSERT INTO Customers(FirstName, LastName)
+    VALUES ('F', 'Flintstone')
+INSERT INTO Customers(FirstName, LastName)
+    VALUES ('Fred', 'F')
+
+*/
+
+-- C) Add an extra bit of information on the Customer table. The client wants to
+--    start tracking customer emails, so they can send out statements for
+--    outstanding payments that are due at the end of the month.
+ALTER TABLE Customers
+    ADD Email varchar(30) NULL
+    -- Adding this as a nullable column, because customers already
+    -- exist, and we don't have emails for those customers.
+GO
+
+-- D) Add indexes to the Customer's First and Last Name columns
+-- Indexes improve the performance of the database when retrieving information.
+CREATE NONCLUSTERED INDEX IX_Customers_FirstName
+    ON Customers (FirstName)
+CREATE NONCLUSTERED INDEX IX_Customers_LastName
+    ON Customers (LastName)
+GO -- End of a batch of instructions
+
+-- E) Add a default constraint on the Orders.Date column to use the current date.
+-- GETDATE() is a global function in the SQL Server Database
+-- GETDATE() will obtain the current date/time on the database server
+IF OBJECT_ID('DF_Orders_Date', 'C') IS NOT NULL
+    ALTER TABLE Orders DROP CONSTRAINT DF_Orders_Date
+
+ALTER TABLE Orders
+    ADD CONSTRAINT DF_Orders_Date
+        DEFAULT GETDATE() FOR [Date]
+--      Use     \ this  / for \this column/ if no value was supplied when INSERTING data
+GO
+-- To illustrate the default value, consider this sample row for the Orders table
+INSERT INTO Orders(CustomerNumber, Subtotal, GST)
+    VALUES (101, 150.00, 7.50)
+-- Select the current orders
+SELECT  OrderNumber, CustomerNumber, Total, [Date]
+FROM    Orders
+GO
+
+-- Prep-data for change request D)....
+-- Some inventory data without a ItemDescription
+INSERT INTO InventoryItems(ItemNumber, ItemDescription, CurrentSalePrice, InStockCount, ReorderLevel)
+    VALUES ('GR35A', NULL, 45.95, 8, 5)
+INSERT INTO InventoryItems(ItemNumber, CurrentSalePrice, InStockCount, ReorderLevel)
+    VALUES ('KD5-Q', 1.45, 10, 7)
+GO
+
+SELECT  ItemNumber, ItemDescription, CurrentSalePrice
+FROM    InventoryItems
+GO
+
+-- F) Change the InventoryItems.ItemDescription column to be NOT NULL
+--    WAIT!! We have described the ItemDescription as allowing NULL values.
+--           That means we might have data in the table where the
+--           ItemDescription doesn't exist.
+--           If we try to make that column NOT NULL, what will we do about
+--           the existing data in the database where it is "empty"??
+--           We can fix that by updating the data in the database
+--           where that description is missing.
+UPDATE      InventoryItems
+   SET      ItemDescription = '-missing-'
+   WHERE    ItemDescription IS NULL
+GO
+-- Also Note: We might be asked to put a default value for the column
+--            that will become required. In this case, let's use a
+--            default value of '-no description-'
+ALTER TABLE InventoryItems
+    ADD CONSTRAINT DF_InventoryItems_Description
+        DEFAULT '-no description-' FOR ItemDescription
+GO
+-- Here's some sample data where ItemDescription is not entered
+-- and the default value comes into play. (We'll do 2 rows at once.)
+INSERT INTO InventoryItems(ItemNumber, CurrentSalePrice, InStockCount, ReorderLevel)
+    VALUES ('B-95R', 45.95, 8, 5),
+           ('GR47D', 92.45, 3, 3)
+GO
+
+--   Now we can change the ItemDescription to be required (NOT NULL)
+ALTER TABLE InventoryItems
+    ALTER COLUMN ItemDescription varchar(50) NOT NULL
+GO
+
+-- Let's enter a bit more information in our database
+INSERT INTO InventoryItems(ItemNumber, ItemDescription, CurrentSalePrice, InStockCount, ReorderLevel)
+    VALUES ('BL-92', '92mm Bolt', 3.95, 18, 6)
+
+-- Let's see the data in the Inventory table
+SELECT  ItemNumber, ItemDescription, CurrentSalePrice
+FROM    InventoryItems
+GO
+
+-- G) Add an indes on the Item's Description column, to improve search.
+CREATE NONCLUSTERED INDEX IX_InventoryItems_ItemDescription
+    ON InventoryItems (ItemDescription)
+
