f37ca7eb2846bdfacc0b716f2118c5144c32ca96
Dan Gilleland <dgilleland@users.noreply.github.com>
2018-11-22 10:33:54 -0700

    New BLL Signatures

--- GL Starter Kit App/GroceryStore/BLL/OrderListsController.cs
+++ GL Starter Kit App/GroceryStore/BLL/OrderListsController.cs
@@ -12,6 +12,7 @@ namespace GroceryStore.BLL
     [DataObject]
     public class OrderListsController
     {
+        #region Query Methods
         public List<OrderItem> OrderLists_OrderPickList(int orderid)
         {
             // Retrieve the items on an order. Only unpicked orders are to be retrieved
@@ -23,14 +24,18 @@ namespace GroceryStore.BLL
         {
             throw new NotImplementedException();
         }
+        #endregion
 
+        #region Command Methods
         public void OrderLists_Picking(int pickingid, int ordereid, List<PickListItem> picklist)
         {
             // Transaction!
             // Update order list item record(s), update order totals, picker and pick date
             // Tables: Orders (U), OrderLists (U)
         }
+        #endregion
 
+        #region Exposes Database Entities (bit extreme for what's needed)
         [DataObjectMethod(DataObjectMethodType.Select)]
         public List<Order> Orders_UnDeliveredList()
         {
@@ -41,7 +46,6 @@ namespace GroceryStore.BLL
         {
             throw new NotImplementedException();
         }
-
-
+        #endregion
     }
 }
